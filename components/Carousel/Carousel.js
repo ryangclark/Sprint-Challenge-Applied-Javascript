4b406ce4d19d57ad8309f80c380fe04e9e7dea67
Clark <rclark010@gmail.com>
2019-02-01 11:29:44 -0700

    STRETCH: Have a basic carousel working.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,11 +1,48 @@
 class Carousel {
-
+    constructor(carousel) {
+        this.carousel = carousel;
+        console.log('class', this.carousel);
+        // get your buttons
+        this.leftButton = this.carousel.getElementsByClassName('left-button')['0'];
+        this.rightButton = this.carousel.getElementsByClassName('right-button')['0'];
+        // add eventListeners to buttons
+        this.leftButton.addEventListener('click', () => this.leftClick());
+        this.rightButton.addEventListener('click', () => this.rightClick());
+        // get your images
+        this.imagesList = this.carousel.querySelectorAll('img');
+        // current index
+        this.currentIndex = 0;
+        this.clickHandler();
+    }
+    leftClick() {
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.imagesList.length - 1;
+        } else {
+            this.currentIndex -= 1;
+        }
+        console.log(this.currentIndex);
+        return this.clickHandler();
+    }
+    rightClick() {
+        if (this.currentIndex == this.imagesList.length - 1) {
+            this.currentIndex = 0;
+        } else {
+            this.currentIndex += 1;
+        }
+        console.log(this.currentIndex);
+        return this.clickHandler();
+    }
+    clickHandler() {
+        this.imagesList.forEach(image => image.style.display = 'none');
+        this.imagesList[this.currentIndex].style.display = 'block';
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.getElementsByClassName('carousel')['0'];
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
